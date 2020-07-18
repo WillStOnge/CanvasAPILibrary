1a8088b5c784db726116b203175fb252b1ed758a
Will St. Onge <ongewill@gmail.com>
2020-07-18 13:09:33 -0400

    Testing complete on 3cd472f (#2)
    * Push changes to testing branch from willst (#1)
    
    * Added base controller class
    * Added quiz controller delete method and empty quiz class for the returned data.
    
    * Update README.md

---
+++ src/cavnas/controllers/Controller.java
@@ -0,0 +1,76 @@
+package cavnas.controllers;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Base class for all controllers in this library.
+ */
+public class Controller
+{
+    protected static String run(Method method, URL endpoint, String token, String jsonParams) throws IOException
+    {
+        // Create new connection to the URL.
+        HttpURLConnection conn = (HttpURLConnection) endpoint.openConnection();
+        conn.setRequestMethod(method.getName());
+        conn.setRequestProperty("Content-Type", "application/JSON");
+        conn.setRequestProperty("Authorization", "Bearer " + token);
+        conn.setDoOutput(true);
+
+        // If there are parameters, send them to the endpoint
+        if (jsonParams != null)
+        {
+            try (OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), "UTF-8"))
+            {
+                writer.write(jsonParams);
+                writer.flush();
+            }
+        }
+
+        StringBuilder response = new StringBuilder();
+
+        // Read the response from the endpoint. If the response code isn't 200, it will throw an IOException
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")))
+        {
+            String s;
+
+            while ((s = in.readLine()) != null)
+                response.append(s);
+        }
+
+        // Return response
+        return response.toString();
+    }
+}
+
+/**
+ * Contains all HTTP methods used in the Canvas API
+ */
+enum Method
+{
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    private final String name;
+
+    Method(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
+
+class EndpointException extends Exception
+{
+    public EndpointException(String s)
+    {
+        super(s);
+    }
+}
---
+++ src/cavnas/controllers/QuizController.java
@@ -0,0 +1,33 @@
+package cavnas.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Controller for all operations on quizzes in Canvas.
+ */
+public class QuizController extends Controller
+{
+    /**
+     * @param canvasUrl URL to your canvas instance (Ex. https://test.instructure.com)
+     * @param token     Bearer token used to authenticate with the Canvas API
+     * @param courseId  Course which contains the desired quiz
+     * @param quizId    Quiz you want to delete
+     * @return Returns true if the deletion was successful, false if not.
+     */
+    public static boolean deleteQuiz(String canvasUrl, String token, Integer courseId, Integer quizId)
+    {
+        try
+        {
+            URL url = new URL(canvasUrl + "/api/v1/courses/" + courseId + "/quizzes/" + quizId);
+            run(Method.DELETE, url, token, null);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
