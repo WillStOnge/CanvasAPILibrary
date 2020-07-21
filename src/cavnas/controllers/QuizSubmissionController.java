d5bfea900ac4d37aa56293de2220d558474d1d51
Wie Lie Sie <wlie.sie@gmail.com>
2020-07-21 18:21:10 -0400

    quizSubmission() fixed

--- src/cavnas/controllers/QuizSubmissionController.java
+++ src/cavnas/controllers/QuizSubmissionController.java
@@ -1,45 +1,92 @@
 package cavnas.controllers;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import cavnas.utils.structs.Quiz;
-import cavnas.utils.structs.QuizSubmissions;
+import cavnas.utils.structs.QuizSubmission;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class QuizSubmissionController extends Controller
 {
-     /**
-     * @param canvasUrl URL to your canvas instance (Ex. https://test.instructure.com)
-     * @param token     Bearer token used to authenticate with the Canvas API
-     * @param courseId  Course which contains the desired quiz
-     * @param quizId    Quiz you want to get
-     * @param submissionId  the submission ID (Ex. 1182288)
-     * @return Returns list of all submissions for this quiz, Users who can view or manage grades for a course will have submissions from multiple users returned. A user who can only submit will have only their own submissions returned. When a user has an in-progress submission, only that submission is returned. When there isn't an in-progress quiz_submission, all completed submissions, including previous attempts, are returned.
+    /**
+     * @param canvasUrl    URL to your canvas instance (Ex. https://test.instructure.com)
+     * @param token        Bearer token used to authenticate with the Canvas API
+     * @param courseId     Course which contains the desired quiz
+     * @param quizId       Quiz you want to get the submission from
+     * @param submissionId Submission you want to retrieve
+     * @return Returns list of all submissions for this quiz. If there is an error, it will return null.
      **/
-    public static QuizSubmissions getQuizSubmission(String canvasUrl, String token, Integer courseId, Integer quizId, Integer submissionId)
+    public static QuizSubmission getQuizSubmission(String canvasUrl, String token, Integer courseId, Integer quizId, Integer submissionId)
     {
         String json;
-        //Type quizSubmissionList = new TypeToken<List<QuizSubmissions>>(){}.getType();
 
         try
         {
             URL url = new URL(canvasUrl + "/api/v1/courses/" + courseId + "/quizzes/" + quizId + "/submissions/" + submissionId);
             json = run(Method.GET, url, token, null);
-        }
-        catch (IOException e)
+        } catch (IOException e)
         {
             e.printStackTrace();
             return null;
         }
 
-        //ArrayList<QuizSubmissions> list = new Gson().fromJson(json, QuizSubmissions.class);
-        System.out.println(json);
-        return new Gson().fromJson(json, QuizSubmissions.class);
+        QuizSubmissions submissions = new Gson().fromJson(json, QuizSubmissions.class);
+
+        if (submissions.quiz_submissions.size() > 0)
+            return submissions.quiz_submissions.get(0);
+        return null;
+    }
+
+    /**
+     * Private inner class for converting JSON into a QuizSubmission object properly.
+     */
+    private class QuizSubmissions
+    {
+        public List<QuizSubmission> quiz_submissions;
+    }
+
+    /**
+     * @param canvasUrl URL to your canvas instance (Ex. https://test.instructure.com)
+     * @param token     Bearer token used to authenticate with the Canvas API
+     * @param courseId  Course which contains the desired quiz
+     * @param quizId    Quiz you want to get the submission from
+     * @return Returns list of all submissions for this quiz based on the user permissions. If there is an error, it will return null.
+     **/
+    public static QuizSubmission getQuizSubmissions(String canvasUrl, String token, Integer courseId, Integer quizId)
+    {
+        String json;
+        Quiz.Permissions permissions = new Quiz().new Permissions();
+        Quiz.LockInfo lockInfo = new Quiz().new LockInfo();
+
+        try
+        {
+            URL url = new URL(canvasUrl + "/api/v1/courses/" + courseId + "/quizzes/" + quizId + "/submissions");
+            json = run(Method.GET, url, token, null);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        QuizSubmissions submissions = new Gson().fromJson(json, QuizSubmissions.class);
+
+        /**
+         * will return all submissions if user has manage or view permission
+         * will return single submission by user who can only submit
+         * will return a specific submission if a user has in_progress submissions
+         * will return all completed submissions including previous attempts when there is no an in_progress quiz_submission
+        **/
+        if ( submissions.quiz_submissions.size() > 0 )
+            if ( permissions.manage || lockInfo.can_view )
+                return (QuizSubmission) submissions.quiz_submissions;
+            else if ( permissions.submit )
+                return submissions.quiz_submissions.get(0);
+            else if (/*use has in_progress submissions*/)
+                return /*that specific submission that is still in_progress*/
+        return null;
 
     }
+
 }
