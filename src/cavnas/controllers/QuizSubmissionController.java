42af5eecfe1f6fed7cadab73d09958f387ccb2e3
Will St. Onge <ongewill@gmail.com>
2020-07-20 23:50:41 -0400

    Fixed getSubmission() method

--- src/cavnas/controllers/QuizSubmissionController.java
+++ src/cavnas/controllers/QuizSubmissionController.java
@@ -1,15 +1,11 @@
 package cavnas.controllers;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
-import cavnas.utils.structs.Quiz;
-import cavnas.utils.structs.QuizSubmissions;
+import cavnas.utils.structs.QuizSubmission;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class QuizSubmissionController extends Controller
 {
@@ -17,14 +13,13 @@ public class QuizSubmissionController extends Controller
      * @param canvasUrl URL to your canvas instance (Ex. https://test.instructure.com)
      * @param token     Bearer token used to authenticate with the Canvas API
      * @param courseId  Course which contains the desired quiz
-     * @param quizId    Quiz you want to get
-     * @param submissionId  the submission ID (Ex. 1182288)
-     * @return Returns list of all submissions for this quiz, Users who can view or manage grades for a course will have submissions from multiple users returned. A user who can only submit will have only their own submissions returned. When a user has an in-progress submission, only that submission is returned. When there isn't an in-progress quiz_submission, all completed submissions, including previous attempts, are returned.
+     * @param quizId    Quiz you want to get the submission from
+     * @param submissionId  Submission you want to retrieve
+     * @return Returns list of all submissions for this quiz. If there is an error, it will return null.
      **/
-    public static QuizSubmissions getQuizSubmission(String canvasUrl, String token, Integer courseId, Integer quizId, Integer submissionId)
+    public static QuizSubmission getQuizSubmission(String canvasUrl, String token, Integer courseId, Integer quizId, Integer submissionId)
     {
         String json;
-        //Type quizSubmissionList = new TypeToken<List<QuizSubmissions>>(){}.getType();
 
         try
         {
@@ -37,9 +32,18 @@ public class QuizSubmissionController extends Controller
             return null;
         }
 
-        //ArrayList<QuizSubmissions> list = new Gson().fromJson(json, QuizSubmissions.class);
-        System.out.println(json);
-        return new Gson().fromJson(json, QuizSubmissions.class);
+        QuizSubmissions submissions = new Gson().fromJson(json, QuizSubmissions.class);
 
+        if (submissions.quiz_submissions.size() > 0)
+            return submissions.quiz_submissions.get(0);
+        return null;
+    }
+
+    /**
+     * Private inner class for converting JSON into a QuizSubmission object properly.
+     */
+    private class QuizSubmissions
+    {
+        public List<QuizSubmission> quiz_submissions;
     }
 }
