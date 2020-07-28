45ccc6d3e215cf5cf694a563da52467333bc11f1
Trey Regruth <Treyregruth@gmail.com>
2020-07-28 09:58:56 -0400

    * added the Get current quiz submission times

--- src/cavnas/controllers/QuizSubmissionController.java
+++ src/cavnas/controllers/QuizSubmissionController.java
@@ -2,6 +2,7 @@ package cavnas.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.ZonedDateTime;
 import java.util.List;
 import cavnas.utils.structs.QuizSubmission;
 import com.google.gson.Gson;
@@ -64,10 +65,44 @@ public class QuizSubmissionController extends Controller
     }
 
     /**
+     * @param canvasUrl URL to your canvas instance (Ex. https://test.instructure.com)
+     * @param token     Bearer token used to authenticate with the Canvas API
+     * @param courseId  Course which contains the desired quiz
+     * @param quizId    Quiz the submission is from
+     * @param submisId  ID of the submission object
+     * @return Returns a QuizSubmissionTime object with the end_at parameter and the time_left parameter
+     */
+    public static QuizSubmissionTime getCurrentQuizSubmissionTimes(String canvasUrl, String token, Integer courseId, Integer quizId, Integer submisId)
+    {
+        String json;
+        try
+        {
+            URL url = new URL(canvasUrl + "/api/v1/courses/" + courseId + "/quizzes/" + quizId + "/submissions" + submisId + "/time");
+            json = run(Method.GET, url, token, null);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        return new Gson().fromJson(json, QuizSubmissionTime.class);
+    }
+
+    /**
      * Private inner class for converting JSON into a QuizSubmission object properly.
      */
     private class QuizSubmissions
     {
         public List<QuizSubmission> quiz_submissions;
+    }
+
+    /**
+     * Public inner class for returning submission times
+     */
+    public class QuizSubmissionTime
+    {
+        public ZonedDateTime end_at;
+        public int time_left;
     }
 }
